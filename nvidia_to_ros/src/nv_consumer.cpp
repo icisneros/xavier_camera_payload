50c0eb7bb694874886032c5f0a57f8e1f6f7e6c4
icisneros <ivanc11235@gmail.com>
2023-03-21 12:58:48 -0400

    changed over to logging using the mp4 method

--- nvidia_to_ros/src/nv_consumer.cpp
+++ nvidia_to_ros/src/nv_consumer.cpp
@@ -19,14 +19,8 @@
 #include <nvidia_to_ros/nv2ros.h>
 
 
-/*
 
-Ivan
-Modifications over the original "nv_consumer.cpp":
-  0) Publishes every other frame, cutting the fps to half of the original framerate (typically 24fps -> 12fps).
-      This helps us produce smaller rosbags. Also, we don't need super fast framerate for localization.
-  1) Publishes an rgb8 rather than rgba8 image. We don't need the alpha channel anyway.
-*/
+
 
 
 
@@ -35,36 +29,78 @@ cv_bridge::CvImage img_bridge;
 bool visualize = false;
 
 
-bool publish_this_frame = true;  //ivan
+
+
 
 void multi_callback(std::vector<nv2ros::NvImageMessage> msgs){
-  if (publish_this_frame){    //ivan
-    for(int i = 0; i < msgs.size(); i++){
-      void* pdata = NULL;
-      msgs[i].mem_map(&pdata);
-      msgs[i].mem_sync_for_cpu(&pdata);
-      
-      cv::Mat img = cv::Mat(msgs[i].get_height(), msgs[i].get_width(), CV_8UC4, pdata, msgs[i].get_pitch());
-      
-      cv::Mat img_rgb8; // ivan
-      cv::cvtColor(img, img_rgb8, CV_BGRA2BGR); //ivan
 
-      sensor_msgs::Image img_msg;
-      std_msgs::Header header;
-      header.stamp = msgs[i].get_stamp();
-      img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, img_rgb8);  //ivan
-      
-      img_bridge.toImageMsg(img_msg);
-      image_pubs[i].publish(img_msg);
+  for(int i = 0; i < msgs.size(); i++){
+    void* pdata = NULL;
+    msgs[i].mem_map(&pdata);
+    msgs[i].mem_sync_for_cpu(&pdata);
     
-      msgs[i].mem_unmap(&pdata);
-    }
-    publish_this_frame = false;
+    cv::Mat img = cv::Mat(msgs[i].get_height(), msgs[i].get_width(), CV_8UC4, pdata, msgs[i].get_pitch());
+    
+    cv::Mat img_rgb8; // ivan
+    cv::cvtColor(img, img_rgb8, CV_BGRA2BGR); //ivan
+
+    sensor_msgs::Image img_msg;
+    std_msgs::Header header;
+    header.stamp = msgs[i].get_stamp();
+    header.frame_id = "camera" + std::to_string(i);  //ivan
+    img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, img_rgb8);  //ivan
+    
+    img_bridge.toImageMsg(img_msg);
+    image_pubs[i].publish(img_msg);
+  
+    msgs[i].mem_unmap(&pdata);
   }
-  else{
-    publish_this_frame = true;
-  }
+
 }
+
+
+
+/*
+Ivan
+Rate limiter (limits to approx. half the normal framerate)
+Modifications over the original "nv_consumer.cpp":
+  0) Publishes every other frame, cutting the fps to half of the original framerate (typically 24fps -> 12fps).
+      This helps us produce smaller rosbags. Also, we don't need super fast framerate for localization.
+  1) Publishes an rgb8 rather than rgba8 image. We don't need the alpha channel anyway.
+*/
+
+// bool publish_this_frame = true;  //ivan
+
+// void multi_callback(std::vector<nv2ros::NvImageMessage> msgs){
+  
+//   if (publish_this_frame){    //ivan
+//     for(int i = 0; i < msgs.size(); i++){
+//       void* pdata = NULL;
+//       msgs[i].mem_map(&pdata);
+//       msgs[i].mem_sync_for_cpu(&pdata);
+      
+//       cv::Mat img = cv::Mat(msgs[i].get_height(), msgs[i].get_width(), CV_8UC4, pdata, msgs[i].get_pitch());
+      
+//       cv::Mat img_rgb8; // ivan
+//       cv::cvtColor(img, img_rgb8, CV_BGRA2BGR); //ivan
+
+//       sensor_msgs::Image img_msg;
+//       std_msgs::Header header;
+//       header.stamp = msgs[i].get_stamp();
+//       header.frame_id = "camera" + std::to_string(i);  //ivan
+//       img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, img_rgb8);  //ivan
+      
+//       img_bridge.toImageMsg(img_msg);
+//       image_pubs[i].publish(img_msg);
+    
+//       msgs[i].mem_unmap(&pdata);
+//     }
+//     publish_this_frame = false;
+//   }
+//   else{
+//     publish_this_frame = true;
+//   }
+// }
 
 
 
