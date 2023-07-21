b016edfd3480f482f46cef01502d378e460fdaec
icisneros <ivanc11235@gmail.com>
2023-07-21 12:04:37 -0400

    after demo.  Commenting out the localization start and stop scripts so that we can run the data collection for the paper in a more streamlined manner.

--- toplevel/scripts/start_scripts.sh
+++ toplevel/scripts/start_scripts.sh
@@ -3,5 +3,5 @@
 # Sensors launch and rosbag record
 gnome-terminal --window --maximize --title="Start Sensors" -- bash -c "tmuxp load /home/airlab/ws/src/toplevel/sensors_start.yaml"
 
-# Launch localization scripts (docker and odom)
-gnome-terminal --window --maximize --title="Start Localization" -- bash -c "sleep 20; tmuxp load /home/airlab/GNSS_Denied_Localization_IMU/src/UAV_VPR_loc/launch/Localization_start.yaml"
+# # Launch localization scripts (docker and odom)
+# gnome-terminal --window --maximize --title="Start Localization" -- bash -c "sleep 20; tmuxp load /home/airlab/GNSS_Denied_Localization_IMU/src/UAV_VPR_loc/launch/Localization_start.yaml"
--- toplevel/scripts/stop_scripts.sh
+++ toplevel/scripts/stop_scripts.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Kill the localization window (from last window to first)
-SESSION="Localization_session"
-WINDOW="Localization_window"
+# # Kill the localization window (from last window to first)
+# SESSION="Localization_session"
+# WINDOW="Localization_window"
 
-for pane_id in 7 6 5 4 2 1 3 0; do
-    tmux send-keys -t ${SESSION}:${WINDOW}.${pane_id} C-b C-c 
-    sleep 2
-done
-sleep 5
-tmux kill-session -t $SESSION
+# for pane_id in 7 6 5 4 2 1 3 0; do
+#     tmux send-keys -t ${SESSION}:${WINDOW}.${pane_id} C-b C-c 
+#     sleep 2
+# done
+# sleep 5
+# tmux kill-session -t $SESSION
 
 
 
